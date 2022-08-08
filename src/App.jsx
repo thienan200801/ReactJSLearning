a23e408d7d2238eb2f8f6c9802b2c50439ac2f45
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2022-08-08 21:49:05 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,376 @@
+// import './App.css';
+// import { gql, useQuery,useMutation, useLazyQuery } from '@apollo/client';
+// import { useCallback, useEffect, useState } from 'react';
+// import { Formik } from 'formik';
+// import { BrowserRouter, Routes, Route, Outlet, useParams, } from "react-router-dom";
+// import Test from './test';
+// import Footer from './footer';
+
+// export default function App(){
+//   return (
+//     <Footer/>
+//   )
+// }
+
+// export default function Modal(){
+//   return (
+//     <Test />
+//   )
+// }
+
+// const GET_LIST_CART_ITEMS = gql`
+//     query GetListCartItems($customerId: ID!) {
+//         customer(customerId: $customerId) {
+//           items {
+//             productId
+//             quantity
+//           }
+//         }
+//     }
+// `
+
+// export default function ListCartItems(){
+//     const [quantity, setQuantity] = useState(0);
+
+//     const handleAdd = useCallback(()=>{
+//         setQuantity(quantity+1);
+//     }, [])
+
+//     // function handleAdd(){
+//     //     setQuantity(quantity+1);
+//     // }
+    
+//     const {data} = useQuery(GET_LIST_CART_ITEMS, {
+//         variables: {
+//             customerId: "an"
+//         },
+//     })
+//     console.log(data)
+
+//     return (<div>
+//         {data?.customer?.items.map((item, index)=>{
+//             setQuantity(item.quantity);
+
+//             return <ul>
+//                 <h1>Result</h1>
+//                 <li key={index}>{item.productId}</li>
+//                 <li key={index+Math.random()}>{item.quantity}</li>
+//                 <button onClick={()=>handleAdd()}>+</button>
+//             </ul>
+//         })}
+//     </div>)
+// }
+
+// const ADD_TO_CART = gql`
+//   mutation AddItems($item: AddToCartInput!) {
+//     addItem(input: $item) {
+//       totalItems
+//     }
+//   }
+// `
+// const GET_CART = gql`
+//   query {
+//     cart(id: 1){
+//       items{
+//         id
+//         unitTotal{
+//           amount
+//         }
+//         quantity
+//       }
+//     }
+//   }
+// `
+
+// function App() {
+//   const [mutate, result] = useMutation(ADD_TO_CART)
+//   const [getCartQuery, resultQuery] = useLazyQuery(GET_CART)
+//   const [itemId, setItemId] = useState("")
+//   const [price, setPrice] = useState(0)
+//   const [quantity, setQuantity] = useState(0)
+
+//   useEffect(() => {
+//     getCartQuery()
+//   }, [getCartQuery])
+
+//   console.log(resultQuery)
+  
+//   const HandleClick = useCallback(() => {
+//     mutate({
+//       variables: {
+//         item: {
+//           cartId: 1,
+//           id: (itemId),
+//           price: parseInt(price),
+//           quantity: parseInt(quantity)
+//         }
+//       }
+//     }).then(() => {
+//       resultQuery.refetch()
+//     })
+//   }, [itemId, mutate, price, quantity, getCartQuery]) 
+
+//   return <div id="container">
+//     <div id="item-list">
+//       {
+//         resultQuery.data?.cart.items.map((item, index)=>(
+//           <div key={index}>{item.id}: ${item.unitTotal.amount} x {item.quantity}</div>
+//         ))
+//       }
+//     </div>
+
+//     <div id="add-item">
+//       <div>ID of item: <input onChange={(e) => {setItemId(e.target.value)}}/> </div>
+//       <div>Price: <input onChange={(e) => {setPrice(e.target.value)}}/> </div>
+//       <div>Quantity: <input onChange={(e) => {setQuantity(e.target.value)}}/> </div>
+//       <button onClick={HandleClick}>Add</button>
+//     </div>
+
+//   </div>
+// }
+
+// // function Home(){
+// //   return <>
+// //     p1
+// //     <Outlet />
+// //   </>
+// // }
+
+// // function SubPage(){
+// //   return "subPage"
+// // }
+
+// // function Contact(){
+// //   return "p2"
+// // }
+
+// // function About(){
+// //   return "p3"
+// // }
+
+// // function VarPage(){
+// //   const paramValue = useParams()
+// //   const pageName = paramValue.pageName
+// //   const pageId = paramValue.pageId
+
+// //   return <div>
+// //     <h1>{pageName}</h1>
+// //     <p>{pageId}</p>
+// //   </div>
+// // }
+
+// // function SearchParam(){
+// //   let [searchParams, setSearchParams] = useSearchParams();
+
+// //   function handleSubmit(event) {
+// //     event.preventDefault();
+// //     let params = serializeFormQuery(event.target);
+// //     setSearchParams(params);
+// //   }
+
+// //   return (
+// //     <div>
+// //       <form onSubmit={handleSubmit}>{/* ... */}</form>
+// //     </div>
+// //   );
+// // }
+
+// // function App(){
+// //   return(
+// //   <BrowserRouter>
+// //     <Routes>
+// //       <Route path="/" element={<Home />} >
+// //         <Route path="/subPage" element={<SubPage/>}></Route>
+// //       </Route>
+// //       <Route path="/baoan" element={<Contact />} />
+// //       <Route path="/andie" element={<About />}/>
+
+// //       <Route path=":pageName/:pageId" element={<VarPage/>} />
+
+// //     </Routes>
+// //   </BrowserRouter>
+// //   )
+// // }
+
+
+
+// import React from 'react';
+// import { useFormik } from 'formik';
+
+//     const validate = values => {
+//         const errors = {};
+//         if (!values.firstName) {
+//             errors.firstName = 'Required';
+//         } else if (values.firstName.length > 15) {
+//             errors.firstName = 'Must be 15 characters or less';
+//         }
+        
+//         if (!values.lastName) {
+//             errors.lastName = 'Required';
+//         } else if (values.lastName.length > 20) {
+//             errors.lastName = 'Must be 20 characters or less';
+//         }
+        
+//         if (!values.email) {
+//         errors.email = 'Required';
+//         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//             errors.email = 'Invalid email address';
+//         }
+        
+//         return errors;
+//     };
+
+//     const SignupForm = () => {
+//         const formik = useFormik({
+//             initialValues: {
+//                 firstName: '',
+//                 lastName: '',
+//                 email: '',
+//             },
+//             validate,
+//             onSubmit: values => {
+//                 // alert(JSON.stringify(values, null, 2));
+//                 console.log(values, typeof values)
+//             },
+//         });
+//         return (
+//         <form onSubmit={formik.handleSubmit}>
+//         <label htmlFor="firstName">First Name</label>
+//         <input
+//             id="firstName"
+//             name="firstName"
+//             type="text"
+//             onChange={formik.handleChange}
+//             value={formik.values.firstName}
+//             />
+//         {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+//         <br />
+        
+//         <label htmlFor="lastName">Last Name</label>
+//         <input
+//             id="lastName"
+//             name="lastName"
+//             type="text"
+//             onChange={formik.handleChange}
+//             value={formik.values.lastName}
+//             />
+//         {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+//         <br />
+
+//         <label htmlFor="email">Email Address</label>
+//         <input
+//             id="email"
+//             name="email"
+//             type="email"
+//             onChange={formik.handleChange}
+//             value={formik.values.email}
+//             />
+//         {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+//         <br />
+//         <button type="submit">Submit</button>
+//         </form>
+//     );
+//     };
+
+// export default SignupForm;
+// import { gql, useMutation } from '@apollo/client';
+
+// // Define mutation
+// const INCREMENT_COUNTER = gql`
+//   # Increments a back-end counter and gets its resulting value
+//   mutation IncrementCounter {
+//     currentValue
+//   }
+// `;
+
+// function MyComponent() {
+//   // Pass mutation to useMutation
+//   const [mutateFunction, { data, loading, error }] = useMutation(INCREMENT_COUNTER);
+// }
+// export default SignupForm;
+
+// import React from 'react';
+//  import { Field, Form, Formik, FormikProps } from 'formik';
+ 
+//  const MyInput = ({ field, form, ...props }) => {
+//    return <input {...field} {...props} />;
+//  };
+ 
+//  const Example = () => (
+//    <div>
+//      <h1>My Form</h1>
+//      <Formik
+//        initialValues={{ email: '', color: 'red', firstName: '', lastName: '' }}
+//        onSubmit={(values, actions) => {
+//          setTimeout(() => {
+//            alert(JSON.stringify(values, null, 2));
+//            actions.setSubmitting(false);
+//          }, 1000);
+//        }}
+//      >
+//        {(props) => (
+//          <Form>
+//            <Field type="email" name="email" placeholder="Email" />
+//            <Field as="select" name="color">
+//              <option value="red">Red</option>
+//              <option value="green">Green</option>
+//              <option value="blue">Blue</option>
+//            </Field>
+ 
+//            <Field name="lastName">
+//              {({
+//                field, // { name, value, onChange, onBlur }
+//                form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+//                meta,
+//              }) => (
+//                <div>
+//                  <input type="text" placeholder="Email" {...field} />
+//                  {meta.touched && meta.error && (
+//                    <div className="error">{meta.error}</div>
+//                  )}
+//                </div>
+//              )}
+//            </Field>
+//            <Field name="lastName" placeholder="Doe" component={MyInput} />
+//            <button type="submit">Submit</button>
+//          </Form>
+//        )}
+//      </Formik>
+//    </div>
+//  );
+
+import React, { useState, useCallback } from 'react'
+
+const storeSet = new Set(); 
+
+function Counter() {
+	const [count, setCount] = useState(0);
+	const [countOther, setCountOther] = useState(0);
+	
+	const increase = () => setCount(count + 1);
+	const decrease = () => setCount(count - 1);
+	
+	const increaseOther = () => setCountOther(countOther + 1);
+	const decreaseOther = () => setCountOther(countOther + 1);
+	
+	storeSet.add(increase);
+	storeSet.add(decrease);
+	storeSet.add(increaseOther);
+	storeSet.add(decreaseOther);
+	
+	console.log(storeSet);
+	
+	return (
+			<>
+				<div>Count: {count}</div>
+				<button onClick={increase}>+</button>
+				<button onClick={increase}>-</button>
+
+				<div>Count other: {countOther}</div>
+				<button onClick={increaseOther}>+</button>
+				<button onClick={decreaseOther}>-</button>
+			</>
+	)
+}
+
+export default Counter;
---
+++ src/footer.jsx
@@ -0,0 +1,79 @@
+import './style-footer.css';
+
+export default function Footer(){
+    return (
+        <div className="footer-wrapper">
+            <div className="cover">
+                <div className="footer-top">
+                    <div className="col-top-section">
+                        <div className="col1">
+                            <div className="col1-content">FIND A STORE</div>
+                            <div className="col1-content">BECOME A MEMBER</div>
+                            <div className="col1-content">SIGN UP FOR EMAIL</div>
+                            <div className="col1-content">SEND US FEEDBACK</div>
+                        </div>
+                        <div className="col2">
+                            <div className="col2-title">GET HELP</div>
+                            <div className="col2-content">Order Status</div>
+                            <div className="col2-content">Delivery</div>
+                            <div className="col2-content">Returns</div>
+                            <div className="col2-content">Payment Options</div>
+                            <div className="col2-content">Contact Us</div>
+                        </div>
+                        <div className="col3">
+                            <div className="col2-title">ABOUT NIKE</div>
+                            <div className="col2-content">News</div>
+                            <div className="col2-content">Careers</div>
+                            <div className="col2-content">Investors</div>
+                            <div className="col2-content">Sustainability</div>
+                        </div>                        
+                    </div>
+                    <div className="icon-section">
+                        <div className="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg>
+                        </div>
+                        <div className="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg>
+                        </div>
+                        <div className="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg>
+                        </div>
+                        <div className="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer-bottom">
+                    <div className="bottom-left">
+                        <div className="location">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>
+                            <div className="location-content">
+                                Vietnam
+                            </div>
+                        </div>
+                        <div className="tag">
+                            © 2022 Nike, Inc. All Rights Reserved
+                        </div>
+                    </div>
+                    <div className="bottom-right">
+                        <div className="right-content">Guides</div>
+                        <div className="right-content">Terms of Sale</div>
+                        <div className="right-content">Terms of Use</div>
+                        <div className="right-content">Nike Privacy Policy</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
